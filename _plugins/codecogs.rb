6ae75eb1eaeae4031bbc01e23a2661f86fe7f879
David Charte <fdavidcl@outlook.com>
2014-04-13 12:27:57 +0200

    Plugin
    Nop, no estaba mal

--- _plugins/codecogs.rb
+++ _plugins/codecogs.rb
@@ -1,6 +1,6 @@
 module TextFilter
   def codecogs(input)
-    desired = input.gsub(/\$\$\s*(.*?)\s*\$\$/, "<div style=\"text-align:center;\"><img src=\"http://latex.codecogs.com/png.latex?\\1\" alt=\"\\1\" /></div>").gsub(/\$\s*(.*?)\s*\$/, "<img src=\"http://latex.codecogs.com/png.latex?\\1\\inline\"/>")
+    input.gsub(/\$\$\s*(.*?)\s*\$\$/, "<div style=\"text-align:center;\"><img src=\"http://latex.codecogs.com/png.latex?\\1\" alt=\"\\1\" /></div>").gsub(/\$\s*(.*?)\s*\$/, "<img src=\"http://latex.codecogs.com/png.latex?\\1\\inline\"/>")
   end
 end
 Liquid::Template.register_filter(TextFilter)
